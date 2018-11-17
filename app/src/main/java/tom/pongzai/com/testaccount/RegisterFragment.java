4a1b21e9f10163d07eaca00fb71b3cb38c019134
LERSON <lerson-@gmail.com>
2018-11-17 15:03:00 +0700

    intent gallery

--- app/src/main/java/tom/pongzai/com/testaccount/RegisterFragment.java
+++ app/src/main/java/tom/pongzai/com/testaccount/RegisterFragment.java
@@ -1,13 +1,18 @@
 package tom.pongzai.com.testaccount;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 
 /**
@@ -24,8 +29,50 @@ public class RegisterFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-//        Create Toolbar
+//      Create Toolbar
+        createToolbar();
+//      avatar Controller
+        avatarController();
 
+
+
+    }// main method
+
+    private void avatarController() {
+        ImageView imageView = getView().findViewById(R.id.imageViewAvatar);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent to gallery
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Please Choose..."), 1);
+            }
+        });
+    }
+
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_register, menu);
+
+
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.itemUpload) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void createToolbar() {
         Toolbar toolbar  = getView().findViewById(R.id.toolsbarRegister);
         ((MainActivity)getActivity()).setSupportActionBar(toolbar);
         ((MainActivity) getActivity()).getSupportActionBar().setTitle("Register");
@@ -40,8 +87,8 @@ public class RegisterFragment extends Fragment {
             }
         });
 
-
-    }// main method
+        setHasOptionsMenu(true);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
