2bf30919991a436f03a58e247ae5be5230d59f1f
LERSON <lerson-@gmail.com>
2018-11-17 14:15:24 +0700

    toolbar navigator

--- app/src/main/java/tom/pongzai/com/testaccount/RegisterFragment.java
+++ app/src/main/java/tom/pongzai/com/testaccount/RegisterFragment.java
@@ -2,7 +2,9 @@ package tom.pongzai.com.testaccount;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +20,28 @@ public class RegisterFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+
+        Toolbar toolbar  = getView().findViewById(R.id.toolsbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle("Register");
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle("Please .......");
+//        navigater icon
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+
+    }// main method
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
